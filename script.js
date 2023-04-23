dafbbc1f30f633d2891ea32d8f1595d117330907
ArrowMin <botbot_aromin@yahoo.com>
2023-04-22 18:23:40 -0700

    Update RPS

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const buttons = document.querySelectorAll("button");
+const UI = document.querySelector(".UI");
+const UI2 = document.querySelector(".UI2");
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3) + 1;
   if (choice == 1) {
@@ -8,7 +12,8 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
-
+let playerWins = 0;
+let computerWins = 0;
 function playRound(playerSelection, computerSelection) {
   playerSelection =
     playerSelection.slice(0, 1).toUpperCase() +
@@ -40,14 +45,44 @@ function playRound(playerSelection, computerSelection) {
     return "Invalid player input.";
   }
   if (winner == 0) {
+    computerWins++;
     return "You lose! " + computerSelection + " beats " + playerSelection + "!";
   } else {
+    playerWins++;
     return "You win! " + playerSelection + " beats " + computerSelection + "!";
   }
 }
-
-for (let i = 0; i < 5; i++) {
-  const playerSelection = prompt("Enter rock paper or scissors");
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-}
+let rounds = 0;
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("press");
+    const computerSelection = getComputerChoice();
+    UI.textContent = playRound(button.textContent, computerSelection);
+    rounds += 1;
+    UI2.textContent =
+      "Rounds Played: " +
+      rounds +
+      "\nPlayer Score = " +
+      playerWins +
+      "\nComputer Score = " +
+      computerWins;
+    if (playerWins == 5) {
+      UI.textContent = "Player Wins!";
+      rounds = 0;
+      playerWins = 0;
+      computerWins = 0;
+    }
+    if (computerWins == 5) {
+      UI.textContent = "Computer Wins!";
+      rounds = 0;
+      playerWins = 0;
+      computerWins = 0;
+    }
+  });
+});
+console.log("working");
+// for (let i = 0; i < 5; i++) {
+//   const playerSelection = prompt("Enter rock paper or scissors");
+//   const computerSelection = getComputerChoice();
+//   console.log(playRound(playerSelection, computerSelection));
+// }
